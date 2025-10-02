84a635535430c86dba5969ff5e6b771cb7c7b4ad
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-10-02 16:15:30 +0300

    Update class DesiredPerformance

--- desired_performance.py
+++ desired_performance.py
@@ -1,9 +1,13 @@
 from performance import Performance
 
 class DesiredPerformance(Performance):
-    def __init__(self, subjects: list[str], grades: list[float], desired_average: float):
+    def __init__(self, subjects: list[str], grades: dict[str, list[float]], desired_averages: dict[str, float], desired_overall: float):
         super().__init__(subjects, grades)
-        self.__desired_average = desired_average
+        self.__desired_averages = desired_averages
+        self.__desired_overall = desired_overall
 
-    def average_grade(self):
-        return self.__desired_average
+    def average_grade_per_subject(self):
+        return self.__desired_averages
+
+    def overall_average(self):
+        return self.__desired_overall
