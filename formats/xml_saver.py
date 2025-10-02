3d07629ee77b8fae44587006ceb4a98e2df5d13f
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-10-02 16:16:29 +0300

    Update calss XmlSaver

--- formats/xml_saver.py
+++ formats/xml_saver.py
@@ -4,21 +4,26 @@ from data_saver import DataSaver
 class XmlSaver(DataSaver):
     def save(self, data: dict, filename: str):
         student_elem = ET.Element("student")
-
         ET.SubElement(student_elem, "full_name").text = data["full_name"]
         ET.SubElement(student_elem, "group_number").text = data["group_number"]
 
         real_perf_elem = ET.SubElement(student_elem, "real_performance")
-        for subj, grade in zip(data["real_performance"]["subjects"], data["real_performance"]["grades"]):
+        for subj in data["real_performance"]["subjects"]:
             subj_elem = ET.SubElement(real_perf_elem, "subject", name=subj)
-            subj_elem.text = str(grade)
-        ET.SubElement(real_perf_elem, "average").text = str(data["real_performance"]["average"])
+            grades_elem = ET.SubElement(subj_elem, "grades")
+            for grade in data["real_performance"]["grades"][subj]:
+                ET.SubElement(grades_elem, "grade").text = str(grade)
+            ET.SubElement(subj_elem, "average").text = str(data["real_performance"]["average_per_subject"][subj])
+        ET.SubElement(real_perf_elem, "overall_average").text = str(data["real_performance"]["overall_average"])
 
         desired_elem = ET.SubElement(student_elem, "desired_performance")
-        for grade in data["desired_performance"]["desired_grades"]:
-            grade_elem = ET.SubElement(desired_elem, "desired_grade")
-            grade_elem.text = str(grade)
-        ET.SubElement(desired_elem, "desired_average").text = str(data["desired_performance"]["desired_average"])
+        for subj in data["real_performance"]["subjects"]:
+            subj_elem = ET.SubElement(desired_elem, "subject", name=subj)
+            grades_elem = ET.SubElement(subj_elem, "desired_grades")
+            for grade in data["desired_performance"]["desired_grades"][subj]:
+                ET.SubElement(grades_elem, "grade").text = str(grade)
+            ET.SubElement(subj_elem, "average").text = str(data["desired_performance"]["average_per_subject"][subj])
+        ET.SubElement(desired_elem, "overall_average").text = str(data["desired_performance"]["overall_average"])
 
         tree = ET.ElementTree(student_elem)
         tree.write(filename, encoding="utf-8", xml_declaration=True)
