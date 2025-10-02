86c4bc39524a27a6324d25ede4f677a5552191de
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-10-02 16:17:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,49 @@ from formats.xml_saver import XmlSaver
 from formats.csv_saver import CsvSaver
 
 class RealPerformance(Performance):
-    def average_grade(self):
-        grades = self.get_grades()
-        if not grades:
+    def average_grade_per_subject(self):
+        averages = {}
+        for subject in self.get_subjects():
+            grades = self.get_grades()[subject]
+            averages[subject] = sum(grades) / len(grades) if grades else 0
+        return averages
+
+    def overall_average(self):
+        all_averages = self.average_grade_per_subject()
+        if not all_averages:
             return 0
-        return sum(grades) / len(grades)
+        return sum(all_averages.values()) / len(all_averages)
 
 def main():
-    # Студент
     student = Student("Ridkovets Serhii", "PD-51")
 
-    # Успішність
     subjects = ["Math", "Physics", "DevOps", "C#"]
-    grades = [85, 90, 84, 96]
+    grades = {
+        "Math": [85, 90, 88, 95],
+        "Physics": [90, 92, 89, 95],
+        "DevOps": [84, 86, 56, 76],
+        "C#": [96, 98, 94, 100, 100]
+    }
     real_perf = RealPerformance(subjects, grades)
 
-    # Бажана успішність
-    desired_grades = [95, 100, 92, 100]
-    desired_average = 98
-    desired_perf = DesiredPerformance(subjects, desired_grades, desired_average)
+    desired_grades = {
+        "Math": [95, 98],
+        "Physics": [100, 98],
+        "DevOps": [92, 95],
+        "C#": [100, 100]
+    }
+    desired_averages = {
+        "Math": 96,
+        "Physics": 99,
+        "DevOps": 93,
+        "C#": 100
+    }
+    desired_overall = 97
+    desired_perf = DesiredPerformance(subjects, desired_grades, desired_averages, desired_overall)
 
-    # Отримання даних
     student_data = StudentData(student, real_perf, desired_perf)
     data_dict = student_data.to_dict()
 
-    # Створення файлів з даними трьох форматів
     base_filename = f"{student.get_full_name().replace(' ', '_')}_{student.get_group_number()}"
     JsonSaver().save(data_dict, base_filename + ".json")
     XmlSaver().save(data_dict, base_filename + ".xml")
