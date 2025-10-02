4ed2dc5fa52c87268f5e09eddb6dc5e34af72dc5
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-10-02 16:17:01 +0300

    Update class CsvSaver

--- formats/csv_saver.py
+++ formats/csv_saver.py
@@ -5,16 +5,17 @@ class CsvSaver(DataSaver):
     def save(self, data: dict, filename: str):
         with open(filename, "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
+            writer.writerow(["Full Name", "Group", "Subject", "Grades", "Average", "Desired Grades", "Desired Average", "Overall Average", "Desired Overall"])
 
-            writer.writerow(["Full Name", "Group", "Subject", "Grade", "Average", "Desired Grade", "Desired Average"])
-
-            for subj, grade in zip(data["real_performance"]["subjects"], data["real_performance"]["grades"]):
+            for subj in data["real_performance"]["subjects"]:
                 writer.writerow([
                     data["full_name"],
                     data["group_number"],
                     subj,
-                    grade,
-                    data["real_performance"]["average"],
-                    ";".join(map(str, data["desired_performance"]["desired_grades"])),
-                    data["desired_performance"]["desired_average"]
+                    ";".join(map(str, data["real_performance"]["grades"][subj])),
+                    data["real_performance"]["average_per_subject"][subj],
+                    ";".join(map(str, data["desired_performance"]["desired_grades"][subj])),
+                    data["desired_performance"]["average_per_subject"][subj],
+                    data["real_performance"]["overall_average"],
+                    data["desired_performance"]["overall_average"]
                 ])
