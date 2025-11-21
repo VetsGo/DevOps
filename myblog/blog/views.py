3cf2b3fc9c20c61fbb48eaf404ac9b35d6df1df0
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-11-21 15:44:10 +0200

    Update views.py

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -27,8 +27,15 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    
+    comments = post.comments.filter(active=True)
+    
+    form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
