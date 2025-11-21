cc90fdab013acd31bb197518360b68b4ccf75bb8
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-11-21 15:10:13 +0200

    Update settings.py

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'springtrapukrainian@gmail.com'
+EMAIL_HOST_PASSWORD = 'hecaetdkxaxhbcwf'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
