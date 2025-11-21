d9271f3da916924dc28df337bd49b6b69a09eff0
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-11-21 08:32:43 +0200

    Update models.py

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -13,7 +13,8 @@ class Post(models.Model):
             PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    publish = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
