756e47fb5a4db1c3109a70a4029d8917c376973c
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-11-28 10:24:14 +0200

    Update urls.py

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,6 +3,7 @@ from django.contrib.sitemaps import Sitemap
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -17,4 +18,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
