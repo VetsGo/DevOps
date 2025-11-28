93f4582f1c01a9893255dfe2b46e6bb9da4c060b
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-11-28 11:29:04 +0200

    Add data to PostgreSql

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'sermoji428',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
