d29d30f2f2d5a8fe4a14adc80f238935182eace0
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-11-28 08:51:14 +0200

    Created tags

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -6,10 +6,16 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
@@ -18,7 +24,7 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
